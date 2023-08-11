047888e497c8d50a8190bd337a8017bb12639a2b
Ershad Qaderi <ershad.qaderi@gmail.com>
2023-08-11 11:03:28 +0200

    Add first page with typescript

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home-page">
+    <header>
+      <h1>{{ state.message }}</h1>
+    </header>
+    <main>
+      <p>{{ state.description }}</p>
+    </main>
+    <footer>
+      <p>{{ state.footerContent }}</p>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+type HomePageState  = {
+  message: string;
+  description: string;
+  footerContent: string;
+}
+
+const state: HomePageState = reactive({
+  message: "Welcome to the Vue 3 + TypeScript + Composition API Homepage",
+  description: "This is the main content of the homepage.",
+  footerContent: "Footer Content",
+});
+
+</script>
+
+<style scoped>
+.home-page {
+  text-align: center;
+}
+header,
+footer {
+  background-color: #f0f0f0;
+  padding: 10px;
+}
+main {
+  padding: 20px;
+}
+</style>
